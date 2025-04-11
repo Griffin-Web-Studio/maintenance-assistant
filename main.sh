f09090855bb2256a6218749ae36741ca043d63f5
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-04-11 14:38:26 +0100

    feat: add check for existing screen

--- main.sh
+++ main.sh
@@ -16,8 +16,14 @@ check_screen_installed
 # Name of the screen session
 SESSION_NAME="gws-maintenance"
 
-# Start a new screen session and run the prep.sh script
-screen -dmS "$SESSION_NAME" ./prep.sh "$@"
-
-# Attach to the screen session
-screen -r "$SESSION_NAME"
+# Check if the screen session already exists
+if screen -list | grep -q "$SESSION_NAME"; then
+    echo "Attaching to existing screen session: $SESSION_NAME"
+    screen -r "$SESSION_NAME"
+else
+    echo "Creating new screen session: $SESSION_NAME"
+    # Start a new screen session and run the prep.sh script
+    screen -dmS "$SESSION_NAME" ./prep.sh "$@"
+    # Attach to the new screen session
+    screen -r "$SESSION_NAME"
+fi
