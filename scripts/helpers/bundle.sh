f7da64335d566540c622bd389b68dde87d96b535
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-04-12 21:39:24 +0100

    chore: create helper and utils bundles

---
+++ scripts/helpers/bundle.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#########################################
+## DO NOT EXECUTE THIS SCRIPT DIRECTLY ##
+#########################################
+
+HELPERS_DIR="$DIR/scripts/helpers"
+
+. "$HELPERS_DIR/env_check.sh"
+. "$HELPERS_DIR/dep_check.sh"
---
+++ scripts/utils/bundle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#########################################
+## DO NOT EXECUTE THIS SCRIPT DIRECTLY ##
+#########################################
+
+UTILS_DIR="$DIR/scripts/utils"
+
+. "$UTILS_DIR/cli.sh"
