b67b6cfce073f4b37f0f82e1d82943fbbebe65fc
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-04-12 21:35:14 +0100

    chore(dependencies): move screen dep check inside dedicated helper file

--- main.sh
+++ main.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Function to check if screen is installed
-check_screen_installed() {
-    if ! command -v screen &> /dev/null; then
-        echo "Screen is not installed. Please install it first."
-        echo "For Debian/Ubuntu, you can run: sudo apt-get install screen"
-        echo "For Red Hat/CentOS, you can run: sudo yum install screen"
-        exit 1
-    fi
-}
-
-# Check if screen is installed
-check_screen_installed
-
 # Name of the screen session
 SESSION_NAME="gws-maintenance"
 
--- scripts/helpers/dep_check.sh
+++ scripts/helpers/dep_check.sh
@@ -3,3 +3,13 @@
 #########################################
 ## DO NOT EXECUTE THIS SCRIPT DIRECTLY ##
 #########################################
+
+# Function to check if screen is installed
+function check_screen_installed() {
+    if ! command -v screen &> /dev/null; then
+        echo "Screen is not installed. Please install it first."
+        echo "For Debian/Ubuntu, you can run: sudo apt-get install screen"
+        echo "For Red Hat/CentOS, you can run: sudo yum install screen"
+        exit 1
+    fi
+}
