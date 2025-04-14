7baefdbb470cbf59576e58193f373cf33b7838ef
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-04-14 23:05:19 +0100

    feat: add ability to center text within a terminal window

---
+++ src/utils/text.py
@@ -0,0 +1,23 @@
+import os
+import math
+from typing import Optional
+
+
+def center_text(text: str, spacer: str = "=", length: Optional[int] = None) -> str:
+    """Center text within length
+
+    Args:
+        text (str): text to be centred
+        spacer (str): spacer character.
+        length (Optional[int], optional): length (overrides auto console width). Defaults to None.
+
+    Returns:
+        str: new string with centred text
+    """
+    ter_len = length or os.get_terminal_size().columns
+    text_len = len(text)
+
+    padding = (ter_len - text_len - 4) / 2
+    padding_text = spacer * math.floor(padding)
+
+    return padding_text + " " + text + " " + padding_text
