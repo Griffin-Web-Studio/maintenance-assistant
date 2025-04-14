846111b52bc0fb59e4b36ec620fb6477022bfc77
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-04-14 23:07:31 +0100

    refactor(upgrade): add early escapes and proper version compare

--- src/upgrade.py
+++ src/upgrade.py
@@ -1,13 +1,21 @@
-from argparse import Namespace
+"""
+Software Self Upgrade script
+"""
+
 import sys
 import os
+from argparse import Namespace
+from packaging.version import Version
 import git
 import git.exc
-from packaging.version import Version
+
 from helpers.cli_params import parse_args
+from utils.text import center_text
 
 
 def main(args: Namespace):
+    print(f"\n{center_text('Fetching Maintenance Script Updates 🔍', spacer='/')}\n")
+
     # Get the current working directory
     repo_path = os.getcwd()
 
@@ -21,14 +29,14 @@ def main(args: Namespace):
     # Check if is a branch
     try:
         current_branch = repo.active_branch
-        print("Detected current branch!")
-        print(f"Current branch: {current_branch}")
-        print("You are currently on a branch that may be ahead of the latest tag.")
+        print("- Detected current branch! 😱")
+        print(f"- Current branch: {current_branch}")
+        print("- You are currently on a branch that may be ahead of the latest tag.")
         print(
-            "Please manually upgrade your software to ensure you are on the latest version."
+            "⚠️  Please manually upgrade your software to ensure you are on the latest version."
         )
         print(
-            "if you are developing, then happy coding time 😊. psst, pss... pSSSt you may want to use `--debug` option"
+            "📝 if you are developing, then happy coding time 😊. psst, pss... pSSSt you may want to use `--debug` option"
         )
 
         # Let's not disturb the flow 😌
@@ -44,40 +52,40 @@ def main(args: Namespace):
     current_commit = repo.head.commit
 
     # Check if the current HEAD is on a tag
-    if current_commit in repo.tags:
-
-        current_tag = next(tag for tag in repo.tags if tag.commit == current_commit)
-        print(f"Current version is on tag: {current_tag}")
-
-        # Get all tags and sort them
-        tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
-        latest_tag = tags[-1] if tags else None
-
-        if latest_tag:
-            print(f"Latest version tag: {latest_tag}")
-
-            # Compare versions
-            current_version = Version(current_tag.name.strip().replace("v", ""))
-            latest_version = Version(latest_tag.name.strip().replace("v", ""))
-
-            if current_version < latest_version:
-                print(
-                    f"Update available: {latest_version} is newer than {current_version}"
-                )
-                # TODO: Add Upgrading mechanism
-                sys.exit(0)
-            else:
-                print("You are on the latest version.")
-
-        else:
-            print("No tags found in the repository. Wh-a... how?????")
-    else:
+    if current_commit not in repo.tags:
         print("You are in a detached HEAD state, not on a branch or tag either.")
 
         # Don't let this continue, clearly a bad state
         while True:
             input("soooo, what now? [TEXT]: ")
             print("yeah didn't really care...")
+
+        return 1
+
+    current_tag = next(tag for tag in repo.tags if tag.commit == current_commit)
+    print(f"- Current version is on tag: {current_tag}")
+
+    # Get all tags and sort them
+    tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
+    latest_tag = tags[-1] if tags else None
+
+    # Check if there is a latest tag
+    if not latest_tag:
+        print("No tags found in the repository. Wh-a... how?????")
+        return 1
+
+    print(f"- Latest version tag: {latest_tag}")
+
+    # Compare versions
+    current_version = Version(current_tag.name.strip().replace("v", ""))
+    latest_version = Version(latest_tag.name.strip().replace("v", ""))
+
+    if current_version < latest_version:
+        print(f"- Update available: {latest_version} is newer than {current_version}")
+        # TODO: Add Upgrading mechanism
+        sys.exit(0)
+    else:
+        print("You are on the latest version.")
 
 
 if __name__ == "__main__":
