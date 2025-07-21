2cee12bb795ffa92efe58c7fbc9481b8b0f799d2
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-07-21 08:25:02 +0000

    chore: remove screen related code

--- scripts/helpers/env_check.sh
+++ scripts/helpers/env_check.sh
@@ -4,46 +4,6 @@
 ## DO NOT EXECUTE THIS SCRIPT DIRECTLY ##
 #########################################
 
-# are we inside a screen session
-function is_screen_session() {
-    if [ -n "$STY" ]; then
-        echo "Running inside a screen session."
-    else
-        echo "Not running inside a screen session. Checking if one exists..."
-
-        # Check if session exists
-        find_screen_session "$@"
-    fi
-}
-
-# Find if there is a screen session, and if so, attach to it
-function find_screen_session() {
-    # Check if the screen session already exists
-    if screen -list | grep -q "$SESSION_NAME"; then
-        echo "Attaching to existing screen session: $SESSION_NAME"
-
-        screen -r "$SESSION_NAME"
-
-        exit 0
-    else
-        activate_screen "$@"
-    fi
-}
-
-# Activate a new virtual screen
-activate_screen() {
-    echo "Creating new screen session: $SESSION_NAME"
-    # Start a new screen session and run the prep.sh script
-    screen -dmS "$SESSION_NAME" "$THIS" "$@"
-    # Attach to the new screen session
-    screen -r "$SESSION_NAME"
-    wait_for_input "End of Maintenance Script pres [ANY] key to quit! [ANY]:"
-    echo "Cleaning Up 🧹"
-    sleep 1
-    clear_lines 14 # clear all text and banner
-    exit 0
-}
-
 # Checks and activate the virtual environment
 activate_venv() {
     if [ -d ".venv" ]; then
