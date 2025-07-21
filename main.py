2c4108f8b4bd3fc90a6eae54a73b0fdb5e943726
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-07-21 08:20:35 +0000

    chore: implement main banner

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from configs.settings import MAIN_BANNER_ARRAY
 from helpers.setup_check import run_updates
+from utils.text import print_message_array
+
+print_message_array(MAIN_BANNER_ARRAY)
 
 run_updates()
