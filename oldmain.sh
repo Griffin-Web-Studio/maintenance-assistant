0ae12e7c18e735c0847f28ecfe00dfebca435f91
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-07-21 08:25:34 +0000

    chore: remove more code from oldmain bash file

--- oldmain.sh
+++ oldmain.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-print_message_array "${main_banner_text_array[@]}"
-
-# Check if required software exists
-check_screen_installed
-check_python_installed
-
-# Open existing or create new screen session
-is_screen_session "$@"
 
 # Activate virtual python environment
 activate_venv
