4c3ba0e5cba48dc4b5841e60a919700e13c310c5
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-07-21 08:30:07 +0000

    feat: introduce assistant script
    Will be initialised in one of the panes of tmux session

---
+++ assistant.py
@@ -0,0 +1,35 @@
+import time
+import libtmux
+
+from configs.settings import SESSION_NAME
+
+
+def countdown(start):
+    # TODO: remove this temp countdown function as it will be replaced by a the actual application logic
+    for i in range(start, -1, -1):
+        print(i)
+        time.sleep(1)
+
+
+def close_tmux_session(session_name) -> None:
+    """Close a tmux session by name.
+
+    Args:
+        session_name (_type_): Name of the tmux session to close.
+    """
+    # Create a new tmux server connection
+    server = libtmux.Server()
+
+    # Find the session by name
+    session = server.find_where({'session_name': session_name})
+
+    if session:
+        session.kill_session()
+        print(f"Session '{session_name}' has been closed.")
+    else:
+        print(f"No session found with the name '{session_name}'.")
+
+
+if __name__ == "__main__":
+    countdown(20)
+    close_tmux_session(SESSION_NAME)
