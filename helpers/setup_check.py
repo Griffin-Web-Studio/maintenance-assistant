65ceeffa28c12a989e003c46a94e5a6fa4c00c9b
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-07-21 08:26:55 +0000

    feat: control tmux from within python

--- helpers/setup_check.py
+++ helpers/setup_check.py
@@ -1,5 +1,10 @@
 import subprocess
+import os
 import sys
+
+import libtmux
+
+from configs.settings import SESSION_NAME
 
 
 def run_updates() -> None:
@@ -28,4 +33,44 @@ def command_installed(command: str, install_instructions: str) -> None:
     except subprocess.CalledProcessError:
         print(f"{command.capitalize()} is not installed. Please install it first.")
         print(install_instructions)
+        sys.exit(1)
+
+
+def in_tmux() -> bool:
+    """Check if the script is running inside a tmux session.
+
+    Returns:
+        bool: True if running inside tmux, False otherwise.
+    """
+    return 'TMUX' in os.environ
+
+
+def init_tmux():
+    server = libtmux.Server()
+    existing_session = server.find_where({'session_name': SESSION_NAME})
+
+    try:
+        if existing_session:
+            existing_session.attach()
+            print(f"Attached to existing session: {SESSION_NAME}")
+        else:
+            # Create a new session
+            session = server.new_session(session_name=SESSION_NAME)
+
+            # Create a new window
+            window = session.active_window
+
+            # Split the window into panes
+            pane1 = window.split_window(vertical=False)  # Horizontal split
+            pane2 = window.split_window(vertical=True)    # Vertical split
+
+            # Send commands to the panes
+            pane1.send_keys('python ./assistant.py')
+            pane2.send_keys('echo "Running in Pane 2"')
+            window.panes[2].send_keys('echo "Running in Pane 3"')
+
+            session.attach()
+
+    except libtmux.libtmux.TmuxException:
+        print("Failed to initialize tmux session, or session ended.")
         sys.exit(1)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from configs.settings import MAIN_BANNER_ARRAY
-from helpers.setup_check import run_updates
+from helpers.setup_check import command_installed, init_tmux, run_updates
 from utils.text import print_message_array
 
 print_message_array(MAIN_BANNER_ARRAY)
@@ -11,3 +11,5 @@ command_installed(
     "For Debian/Ubuntu, you can run: sudo apt-get install tmux\n"
     "For Red Hat/CentOS, you can run: sudo yum install tmux"
 )
+
+init_tmux()
