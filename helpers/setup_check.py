aafdbbb695ce3d1f04a4d94be46c0d15011046b5
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-07-22 20:00:22 +0000

    style(setup_check): format docstring and error messages for better readability

--- helpers/setup_check.py
+++ helpers/setup_check.py
@@ -27,16 +27,24 @@ def command_installed(command: str, install_instructions: str) -> None:
 
     Args:
         command (str): The command to check for installation.
-        install_instructions (str): Instructions for installing the command if it is not found.
+        install_instructions (str): Instructions for installing the command if
+        it is not found.
     """
 
     try:
-        subprocess.run(['which', command],
-                       stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        subprocess.run(
+            ['which', command],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=True
+        )
         print(f"{command.capitalize()} is installed.")
 
     except subprocess.CalledProcessError:
-        print(f"{command.capitalize()} is not installed. Please install it first.")
+        print(
+            command.capitalize(),
+            " is not installed. Please install it first."
+        )
         print(install_instructions)
         sys.exit(1)
 
