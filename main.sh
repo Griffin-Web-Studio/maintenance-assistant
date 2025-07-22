ff29430038a27eb20c3cb9dfb8e95878db6c0ea3
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-07-22 21:56:11 +0000

    revert(main.sh): add back core main.sh code for backward compat

--- main.sh
+++ main.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Full path of the current script
+THIS=$(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null || echo $0)
+
+# The directory where current script resides
+DIR=$(dirname "${THIS}")
+
+# import bundles
+source "$DIR/scripts/helpers/env_check.sh"
+
 # Activate virtual python environment
 activate_venv
 
