01a4f2401a27e63783ec5d67e33655e0e10710c5
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-07-31 23:14:26 +0000

    feat: introduce application config yaml file

--- configs/settings.py
+++ configs/settings.py
@@ -2,29 +2,25 @@
 Griffin Web Studio Maintenance Assistant application. This module defines
 various constants used throughout the application."""
 from pathlib import Path
-
+from typing import List
+import yaml
 from helpers.lock_manager import LockManager
 
 ROOT_DIR: Path = Path(__file__).resolve().parent.parent
-APP_NAME = "Griffin Web Studio - Maintenance Assistant"
-SESSION_NAME = "gws-maintenance"
 MAINTENANCE_LOCK = ROOT_DIR / "maintenance.lock"
 
 # Create an instance of LockManager
 lock_mgr = LockManager(MAINTENANCE_LOCK)
 
 LOCK_TIMESTAMP = lock_mgr.get_lock_timestamp()
-LOG_FILE = ROOT_DIR / "logs" / f"gws-maintenance-{LOCK_TIMESTAMP}.log"
+LOG_FILE = ROOT_DIR / "logs" / f"maintenance-{LOCK_TIMESTAMP}.log"
 
-MAIN_BANNER_ARRAY = [
-    '',
-    '       >>01000111010101110101001100100000010011010010000000110',
-    '      >1   _______       ________                          00',
-    '     00   / ____/ |     / / ____/    MaiNt  aNCe          00',
-    '    01   / / __ | | /| / /\\__ \\      MAI TENA CE         01',
-    '   10   / /_/ / | |/ |/ /___/ /      Ma   En  CE        10',
-    '  00    \\____/  |__/|__/_____/       Ma       CE  01   00',
-    ' 00                                                   1<',
-    '01100000001001011001000000100110010101110101011100010<<',  # slant
-    '',
-]
+
+# Customisation settings
+with open(ROOT_DIR / 'config.yml', 'r') as file:
+    config = dict(yaml.safe_load(file)).get('config', {})
+
+APP_NAME = config.get('app_name', "Griffin Web Studio - Maintenance Assistant")
+SESSION_NAME = config.get('session_name', "gws-maintenance")
+MAIN_BANNER_ARRAY: List[str] = config.get(
+    'main_banner', 'GWS Maintenance Assistant - Banner Fallback').splitlines()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from configs.settings import MAIN_BANNER_ARRAY
-from helpers.setup_check import command_installed, init_tmux, run_updates, venv_activated
+from helpers.setup_check import (
+    command_installed, init_tmux, run_updates, venv_activated)
 from utils.text import print_message_array
 
 print_message_array(MAIN_BANNER_ARRAY)
