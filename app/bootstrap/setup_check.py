26bbce779c674e803ece436268446640daa116f7
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-08-09 18:10:30 +0000

    chore: move setup_check inside app/bootstrap

--- app/bootstrap/setup_check.py
+++ app/bootstrap/setup_check.py
@@ -37,34 +37,7 @@ def run_updates() -> None:
         sys.exit(1)
 
 
-def command_installed(command: str, install_instructions: str) -> None:
-    """Check if a command is installed on the system.
-
-    Args:
-        command (str): The command to check for installation.
-        install_instructions (str): Instructions for installing the command if
-        it is not found.
-    """
-
-    try:
-        subprocess.run(
-            ['which', command],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            check=True
-        )
-        print(f"{command.capitalize()} is installed.")
-
-    except subprocess.CalledProcessError:
-        print(
-            command.capitalize(),
-            " is not installed. Please install it first."
-        )
-        print(install_instructions)
-        sys.exit(1)
-
-
-def in_tmux() -> bool:
+def tmux_activated() -> bool:
     """Check if the script is running inside a tmux session.
 
     Returns:
