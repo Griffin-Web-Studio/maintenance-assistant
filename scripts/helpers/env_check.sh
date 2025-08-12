3023220b61c9a3c73d84ab0811d2a61c279e1141
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-08-12 08:11:13 +0000

    style: improving console readability for venv console printout

--- scripts/helpers/env_check.sh
+++ scripts/helpers/env_check.sh
@@ -10,27 +10,27 @@ activate_venv() {
 
     while [ $attempts -lt $max_attempts ]; do
         if [ -d "$DIR/.venv" ] && [ -f "$DIR/.venv/bin/activate" ]; then
-            echo "Activating virtual environment..."
+            echo "⚙️  Activating virtual environment..."
             source "$DIR/.venv/bin/activate"
             return 0
         else
-            echo "Virtual environment not found or activation script missing."
-            echo "Attempting to create a new virtual environment..."
+            echo "❌  Virtual environment not found or activation script missing."
+            echo "🆕  Attempting to create a new virtual environment..."
             rm -rf "$DIR/.venv"
             echo "‼️ Pay close attention to the output below, as it may contain important information. ‼️"
             python3 -m venv "$DIR/.venv"
         fi
 
         if [ -f "$DIR/.venv/bin/activate" ]; then
-            echo "Activating virtual environment..."
+            echo "⚙️  Activating virtual environment..."
             source "$DIR/.venv/bin/activate"
             return 0
         fi
 
         attempts=$((attempts + 1))
-        echo "Attempt $attempts failed. Retrying..."
+        echo "🔄  Attempt $attempts failed. Retrying..."
     done
 
-    echo "Failed to activate the virtual environment after $max_attempts attempts."
+    echo "☠️  Failed to activate the virtual environment after $max_attempts attempts."
     exit 1
 }
