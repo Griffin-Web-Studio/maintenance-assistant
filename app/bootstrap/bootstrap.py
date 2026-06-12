935d7f85cd0cdaf4c4d8462a10d5ee51812e46b6
Rihards Simanovics <rihards.s@griffin-web.studio>
2026-06-12 16:24:25 +0000

    chore(git-sentinel): commit stashed work

--- app/bootstrap/bootstrap.py
+++ app/bootstrap/bootstrap.py
@@ -1,9 +1,13 @@
-""" bootstrap - just a bootstrap file :)
-"""
+"""bootstrap - just a bootstrap file :)"""
 
 import os
 import sys
-from app.bootstrap.setup_check import init_tmux, run_updates, tmux_activated, venv_activated
+from app.bootstrap.setup_check import (
+    init_tmux,
+    run_updates,
+    tmux_activated,
+    venv_activated,
+)
 from app.configs.constants import MAIN_BANNER_ARRAY, ROOT_DIR
 from app.utils.argument_parser import ArgumentParser
 from app.utils.command_runner import CommandRunner
@@ -11,14 +15,27 @@ from app.utils.text import print_message_array
 from app.assistant import assistant
 from app.queue_worker import queue_worker
 
-
 cmd = CommandRunner()
 
 
 def bootstrap():
     """Bootstrap"""
 
-    print("✅ Python Maintenance bootstrap loaded correctly.\n")
+    print("✅  Python Maintenance bootstrap loaded correctly.\n")
+
+    if not cmd.can_execute("tmux"):
+        print(
+            "For Debian/Ubuntu, you can run: sudo apt-get install tmux\n"
+            "For Red Hat/CentOS, you can run: sudo yum install tmux"
+        )
+        sys.exit(1)
+
+    if not venv_activated():
+        print(
+            "Virtual environment is not activated. "
+            "Please activate it before running this script."
+        )
+        sys.exit(1)
 
     print_message_array(MAIN_BANNER_ARRAY)
     print("")
@@ -34,25 +51,34 @@ def bootstrap():
             if tmux_activated() and venv_activated():
                 assistant()
             else:
-                print("Either TMUX of VENV are not activated!\n"
-                      "See printout below:\n"
-                      f"tmux active: {tmux_activated()}\n"
-                      f"venv active: {venv_activated()}\n")
+                print(
+                    "Either TMUX of VENV are not activated!\n"
+                    "See printout below:\n"
+                    f"tmux active: {tmux_activated()}\n"
+                    f"venv active: {venv_activated()}\n"
+                )
             return
 
         elif args.run == "worker":
             if tmux_activated() and venv_activated():
                 queue_worker()
             else:
-                print("Either TMUX of VENV are not activated!\n"
-                      "See printout below:\n"
-                      f"tmux active: {tmux_activated()}\n"
-                      f"venv active: {venv_activated()}\n")
+                print(
+                    "Either TMUX of VENV are not activated!\n"
+                    "See printout below:\n"
+                    f"tmux active: {tmux_activated()}\n"
+                    f"venv active: {venv_activated()}\n"
+                )
             return
 
-        elif args.run == "legacy" and getattr(args, 'legacy', False) == "assistant":
+        elif (
+            args.run == "legacy"
+            and getattr(args, "legacy", False) == "assistant"
+        ):
             os.execv(
-                "/bin/bash", ['bash', f'{ROOT_DIR}/assistant.legacy.sh'] + sys.argv[1:])
+                "/bin/bash",
+                ["bash", f"{ROOT_DIR}/assistant.legacy.sh"] + sys.argv[1:],
+            )
             sys.exit(0)
 
         print("Run what? Please re-run use -h for more info")
@@ -60,18 +86,5 @@ def bootstrap():
 
     if not args.skip_updates_check:
         run_updates()
-
-    if not cmd.can_execute('tmux'):
-        print(
-            "For Debian/Ubuntu, you can run: sudo apt-get install tmux\n"
-            "For Red Hat/CentOS, you can run: sudo yum install tmux")
-        sys.exit(1)
-
-    if not venv_activated():
-        print(
-            "Virtual environment is not activated. "
-            "Please activate it before running this script."
-        )
-        sys.exit(1)
 
     init_tmux()
--- app/configs/config_file.py
+++ app/configs/config_file.py
@@ -1,9 +1,75 @@
-""" config_file - This module houses the configuration file initialisation logic
+"""config_file - This module houses the configuration file initialisation logic
 for the Griffin Web Studio Maintenance Assistant. It ensures that the
 configuration file exists and is readable, copying a sample file if necessary.
 """
+
 import os
+import yaml
 from pathlib import Path
+from typing import Union, Dict, List, Optional
+
+
+def load_yaml(file_path: str) -> Optional[Union[Dict, List]]:
+    with open(file_path, "r") as file:
+        return yaml.safe_load(file)
+
+
+def write_yaml(file_path: str, data: dict) -> Optional[Union[Dict, List]]:
+    with open(file_path, "w") as file:
+        yaml.dump(data, file, default_flow_style=False)
+
+
+def check_and_set_instance_type(data: any, config_dir: Path) -> Dict:
+    """Check and set the instance type in the configuration.
+
+    This function checks if the instance_type property exists in the config.
+    If not, it prompts the user to specify it and updates the config accordingly.
+
+    Args:
+        data (any): raw yaml data
+        config_dir (Path): path to a yaml config file
+
+    Returns:
+        Dict: The updated configuration dictionary
+    """
+
+    if isinstance(data, dict):
+        print("📜  Configuration file is present and readable.")
+        print("🔍️  Checking where the script is executed")
+        config = data.get("config")
+
+        if config is None:
+            config = {}
+
+        instance_type = config.get("instance_type")
+
+        while instance_type not in ["client", "server"]:
+            instance_type = (
+                input("Please specify the instance type (client/server): ")
+                .strip()
+                .lower()
+            )
+            if instance_type not in ["client", "server"]:
+                print("Invalid input. Please enter 'client' or 'server'.")
+
+            # Update the configuration with the new instance type
+            config["instance_type"] = instance_type
+            print(
+                f"✅  Updated configuration with instance_type: {instance_type}"
+            )
+
+            data["config"] = config
+
+            # Write the updated configuration back to the file
+            write_yaml(config_dir, data)
+
+        print(
+            "☁️  Server - detected"
+            if instance_type == "server"
+            else "🖥️  Client - detected"
+        )
+
+    return config
 
 
 def init_config_file(root_dir: Path) -> None:
@@ -24,12 +90,22 @@ def init_config_file(root_dir: Path) -> None:
         print(
             f"🫗  Configuration file {config_file}",
             "does not exist or is not readable.",
-            f"\n📖  Copying sample config file to {config_file}.")
+            f"\n📖  Copying sample config file to {config_file}.",
+        )
 
         with (
-            open(config_file_sample, 'r') as src,
-            open(config_file, 'w') as dst
+            open(config_file_sample, "r") as src,
+            open(config_file, "w") as dst,
         ):
             dst.write(src.read())
 
-    print("📜  Configuration file is present and readable.")
+    data = load_yaml(root_dir / "config.yml")
+
+    return check_and_set_instance_type(data, config_file)
+
+    raise ValueError(
+        data,
+        """Does not conform to expected data type in config.yml.
+        Please double check that the format of the file with
+        config.sample.yml""",
+    )
